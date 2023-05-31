2d827805783668c2b9d0411ad8b2b160335e52ae
Simon Stauss <stauss.simon.01@gmail.com>
2023-05-31 21:15:50 +0200

    install build dependencies

--- setup/scripts/setup_os.sh
+++ setup/scripts/setup_os.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
 log_file="logs/setup_os.log"
+rm -f "${log_file}"
 
 function setup_linux {
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  echo "Installing build essentials"
+  echo "Setting up build environment..."
   # shellcheck disable=SC2024
-  sudo apt install -y build-essential &> "${log_file}"
+  {
+    sudo apt update && sudo apt -y upgrade
+    sudo apt install -y build-essential  libssl-dev zlib1g-dev libbz2-dev \
+      libreadline-dev libsqlite3-dev curl  libncursesw5-dev xz-utils tk-dev \
+      libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+  } &>> "${log_file}"
 }
 
 function setup_mac {
   echo "Upgrading bash..."
-  brew install bash
+  brew install bash &>> "${log_file}"
   bash_path="$(which bash)"
   echo "${bash_path}" | sudo tee /etc/shells > /dev/null
+
+  echo "Setting up build environment..."
+  brew install openssl readline sqlite3 xz zlib tcl-tk &>> "${log_file}"
 }
 
 # Use homebrew on both linux and mac
@@ -28,4 +37,4 @@ case "${OS}" in
 esac
 
 echo "Installing GNUPG (might take some time)..."
-brew install gnupg &> "${log_file}"
+brew install gnupg &>> "${log_file}"
