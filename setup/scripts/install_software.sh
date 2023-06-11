0a60b4e111c6620a1f2c057ff90a4a352c8303ef
Simon Stauss <stauss.simon.01@gmail.com>
2023-06-11 15:10:43 +0200

    fixed openssl linuxbrew issues

--- setup/scripts/install_software.sh
+++ setup/scripts/install_software.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+log_file="logs/install_software.log"
+rm -f "${log_file}"
+
 function brew_install {
   software_name="$1"
-  software_command="$2"
+  software_command="${2:-${software_name}}"
   if ! command -v "${software_command}" &> /dev/null; then
     echo "Installing ${software_name}..."
-    brew install "${software_name}"
+    brew install "${software_name}" &> "${log_file}"
   fi
 }
 
@@ -17,10 +20,31 @@ function mac_specific {
   brew_install gnu-sed
 }
 
+echo "Updating brew formulae..."
+brew update &> "${log_file}"
 
-brew install python@3.11
-brew_install jenv jenv
-brew_install kubectl kubectl
+echo "Setting up Python (v3.11 via pyenv)..."
+{
+  brew_install pyenv
+  brew install pyenv-ccache
+
+  # Temporary fix for OpenSSL build problems
+  if [[ "${OS}" == "Linux" ]]; then
+    brew remove --ignore-dependencies pkg-config &> /dev/null
+  fi
+  pyenv install 3.11
+  if [[ "${OS}" == "Linux" ]]; then
+      brew install pkg-config &> /dev/null
+  fi
+
+  pyenv global 3.11
+  brew_install pyenv-virtualenv
+  git clone https://github.com/pyenv/pyenv-update.git "$(pyenv root)/plugins/pyenv-update"
+  git clone https://github.com/pyenv/pyenv-doctor.git "$(pyenv root)/plugins/pyenv-doctor"
+} &> "${log_file}"
+
+brew_install jenv
+brew_install kubectl
 
 # Specific software
 case "${OS}" in
--- setup/scripts/setup_os.sh
+++ setup/scripts/setup_os.sh
@@ -9,7 +9,7 @@ function setup_linux {
   # shellcheck disable=SC2024
   {
     sudo apt update && sudo apt -y upgrade
-    sudo apt install -y build-essential  libssl-dev zlib1g-dev libbz2-dev \
+    sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
       libreadline-dev libsqlite3-dev curl  libncursesw5-dev xz-utils tk-dev \
       libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
   } &>> "${log_file}"
